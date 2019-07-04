63bb0f78eb8178b4400400547a7a4dfb79a2eeb1
kenny-tech <kenny4real2001@gmail.com>
2019-07-04 12:01:38 +0100

    Created products and title

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import Product from './Product';
+import Title from './Title';
+import {storeProducts} from '../data';
 
 export default class ProductList extends Component {
+    state = {
+        products: storeProducts
+    }
     render() {
+        console.log(this.state.products);
         return (
-            <div>
-                <Product />
-                <h3>Hello from Product List</h3>
-            </div>
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <div className="row">
+                            <Title name="our" title="products"/>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
