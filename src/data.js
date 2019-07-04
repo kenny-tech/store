ac50bdbc8a48b01127770627f2ec718ef2e5d05e
kenny-tech <kenny4real2001@gmail.com>
2019-07-04 17:53:33 +0100

    Created products

--- src/data.js
+++ src/data.js
@@ -69,7 +69,7 @@ export const storeProducts = [
     {
         id: 7,
         title: "Product 7",
-        img: "img/product-1.png",
+        img: "img/product-7.png",
         price: 10,
         company: "Konga",
         info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
