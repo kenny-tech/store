3035d76839466d3c7b1ff13d0e698079e22ffcfd
kenny-tech <kenny4real2001@gmail.com>
2019-07-04 15:07:08 +0100

    Created context api for products

--- src/data.js
+++ src/data.js
@@ -87,28 +87,17 @@ export const storeProducts = [
         inCart: false,
         count: 0,
         total: 0
-    },
-    {
-        id: 9,
-        title: "Product 9",
-        img: "img/product-9.png",
-        price: 10,
-        company: "Konga",
-        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        inCart: false,
-        count: 0,
-        total: 0
-    },
-    {
-        id: 10,
-        title: "Product 10",
-        img: "img/product-10.png",
-        price: 10,
-        company: "Jumia",
-        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        inCart: false,
-        count: 0,
-        total: 0
     }
-    
 ];
+
+export const detailProduct = {
+    id: 1,
+    title: "Google Pixel - Black",
+    img: "img/product-1.png",
+    price: 10,
+    company: "google",
+    info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    inCart: false,
+    count: 0,
+    total: 0
+};
