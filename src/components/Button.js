d55b25949342ec36e12d462cd9e825bb10f0cb9c
kenny-tech <kenny4real2001@gmail.com>
2019-07-09 17:27:43 +0100

    Updated product page

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ export const ButtonContainer = styled.button`
     background: transparent;
     border: 0.05rem solid var(--lightBlue);
     color: var(--lightBlue);
-    border-radious: 0.5rem;
+    border-radius: 0.5rem;
     padding: 0.2rem 0.5rem;
     cursor: pointer;
     margin: 0.2rem 0.5rem 0.2rem 0;
