37562573e60a5fa4605d0102c4fe68a46719fc9e
kenny-tech <kenny4real2001@gmail.com>
2019-07-11 16:33:04 +0100

    Set product initial state

--- src/context.js
+++ src/context.js
@@ -6,7 +6,7 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state = {
-        products:storeProducts,
+        products:[],
         detailProduct:detailProduct
     }
     componentDidMount() {
