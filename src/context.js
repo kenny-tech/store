453538154e1102d38d1a6fe7bbb4f73641231d97
kenny-tech <kenny4real2001@gmail.com>
2019-08-25 22:17:54 +0100

    Implemented decrement button function

--- src/context.js
+++ src/context.js
@@ -47,11 +47,33 @@ class ProductProvider extends Component {
         },() => {
             this.addTotals();
         }
-      )
-    }
+      );
+    };
 
     decrement = (id) => {
-        console.log('this is decrement method')
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id === id)
+        // get the index of the product
+        const index = tempCart.indexOf(selectedProduct);
+        // get the specific product with index
+        const product = tempCart[index];
+   
+        product.count = product.count - 1;
+
+        if(product.count == 0) {
+            this.removeItem(id);
+        }
+        else {
+            product.total = product.count * product.price;
+            this.setState(()=> {
+                return {
+                    cart: [...tempCart]
+                };
+            },() => {
+                this.addTotals();
+            }
+          );    
+        }
     }
 
     removeItem = (id) => {
